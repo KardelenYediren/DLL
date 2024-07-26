b2a60f466ba8d42498103c85655e6a5d1629d518
The-Kard <212802013@ogr.cbu.edu.tr>
2024-07-26 10:41:56 +0300

    dll dll dll

---
+++ GeneralSettingsLibrary/GeneralSettingsLibrary/GeneralSettingsControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GeneralSettingsLibrary
+{
+    public partial class GeneralSettingsControl : UserControl
+    {
+        public GeneralSettingsControl()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for the Minimize button
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to minimize the parent window
+            if (Window.GetWindow(this) is Window parentWindow)
+            {
+                parentWindow.WindowState = WindowState.Minimized;
+            }
+        }
+
+        // Event handler for the Maximize/Restore button
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to toggle between Maximize and Normal window states
+            if (Window.GetWindow(this) is Window parentWindow)
+            {
+                parentWindow.WindowState = parentWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            }
+        }
+
+        // Event handler for the Close button
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to close the parent window
+            if (Window.GetWindow(this) is Window parentWindow)
+            {
+                parentWindow.Close();
+            }
+        }
+    }
+}
